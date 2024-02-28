a1a0ee373a971658ecc81159fcf44426cbfa357b
Sakthi12345678900 <160562458+Sakthi12345678900@users.noreply.github.com>
2024-02-28 03:21:19 +0000

    rectified submission

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-
 seconds = input('Enter the number of seconds(integer): ')
-seconds=int(seconds)
-minutes,seconds=divmod(seconds,60)
-hours,minutes=divmod(minutes,60)
-print('The duration is',hours,'hours,',minutes,'minutes and',seconds,'seconds')
-
+seconds = int(seconds)
+minutes, seconds = divmod(seconds, 60)
+hours, minutes = divmod(minutes, 60)
+print('The duration is', hours, 'hours,', minutes, 'minutes, and', seconds, 'seconds.')
